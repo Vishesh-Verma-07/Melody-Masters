b5e464c50a164898eefd1792c46f03e1efe2f5b1
Vishesh Verma <visheshverma250703@gmail.com>
2024-06-15 23:40:27 +0530

    : Add mini-svg-data-uri npm dependency and MelodyMasterTestimonial component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+import MelodyMasterTestimonial from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+UpcomingWebinars
 
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MelodyMasterTestimonial />
+      <UpcomingWebinars />
       
     </main>
   );
